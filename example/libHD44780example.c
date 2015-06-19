369604b293d7bf7584fb68abf90a5559aa04c0ef
positronic57 <positronic57@lab>
2015-06-20 00:32:14 +0200

    Update of the LCD init function. Added initial support for 16x4 LCD modules.

--- example/libHD44780example.c
+++ example/libHD44780example.c
@@ -9,6 +9,14 @@
  * the Free Software Foundation; either version 2 of the License.
  *
  * Example of using HD44780 library on Atmega32.
+ * Connection between MCU and LCD:
+ * PORTB0 -> LCD D4
+ * PORTB1 -> LCD D5
+ * PORTB2 -> LCD D6
+ * PORTB3 -> LCD D7
+ * PORTD4 -> LCD RS
+ * PORTD6 -> LCD R/W
+ * PORTD7 -> LCD E
  *
  */ 
 
@@ -24,13 +32,18 @@ int main(void)
 	TSHD44780 HD44780Display;
 	TSHD44780 *pHD44780Display = &HD44780Display;
 	
-	LCDInit4(pHD44780Display,&PORTD,&PORTB,PD4,PD7,PD6,TWO_LINE_DISPLAY);
+	LCDInit4(pHD44780Display,&PORTD,&PORTB,PD4,PD7,PD6,TWO_LINES_DISPLAY);
+	LCDSendCommand4(pHD44780Display,DISPLAY_ON); 
+
 	LCDShowString4(pHD44780Display,"libHD44780");
-	LCDSendCommand4(pHD44780Display,NEW_LINE);
+	LCDSendCommand4(pHD44780Display,LCD16x2_SELECT_LINE_2);
 	LCDShowString4(pHD44780Display,"Tester!!!");
 	
+	LCDSendCommand4(pHD44780Display,CURSOR_ON);
+	LCDSendCommand4(pHD44780Display,BLINKING_CURSOR_ON);
+	
 	while(1)
-    {
+	{
         
-    }
+	}
 }
